5527ffa141a36f5e265f1c4b396fad86be9a8a2e
jm <giantsloar@users.noreply.github.com>
2015-04-10 10:42:53 -0500

    adding fluid.css and super basic html

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <title>Title</title>
+ 
+  <link rel="stylesheet" href="reset.css"/>
+  <link rel="stylesheet" href="fluid.css"/>
+ 
+</head>
+ 
+<body>
+
+<div class="section group">
+	<div class="col span_1_of_4">
+	1 of 4
+	</div>
+	<div class="col span_1_of_4">
+	1 of 4
+	</div>
+	<div class="col span_1_of_4">
+	1 of 4
+	</div>
+	<div class="col span_1_of_4">
+	1 of 4
+	</div>
+</div>
+  
+</body>
+</html>
